21424ed1773c44bde3c2a54171ae45344cbd0b3b
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2025-11-04 13:04:01 +0600

    Изучено конвертация типов данных с String до любых типов данных, с помощью класса Convert. Также попробован класс NumberFromatInfo из библиотеки System.Globalization.

---
+++ ClassConvert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "5";
+
+            int str1ConvertInt = Convert.ToInt32(str1);
+
+            string str2 = "10";
+
+            int str2ConvertInt = Convert.ToInt32(str2);
+
+            Console.WriteLine(str1ConvertInt + str2ConvertInt);
+
+
+            string str3 = "1.25";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+             
+            double str3ToDouble = Convert.ToDouble(str3,numberFormatInfo);
+
+            Console.WriteLine(str3ToDouble + str3ToDouble);
+
+
+        }
+    }
+}
---
+++ ClassConvert/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ClassConvert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassConvert")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e1679434-58d4-4c8e-ba29-ac5f77f791be")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
